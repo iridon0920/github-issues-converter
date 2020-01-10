e6ca6ad0718f171a7286132e224c9407cd98d87d
iridon0920 <iridon0920@gmail.com>
2020-01-10 16:55:28 +0900

    first commit

---
+++ importer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App;
+use Dotenv\Dotenv;
+use SplFileObject;
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+class IssuesImporter
+{
+    private $Token;
+    private $Url;
+
+    public function __construct(string $token, string $repository)
+    {
+        $this->Token = $token;
+        $this->Url = 'https://api.github.com/repos/' . $repository . '/issues';
+    }
+
+    public function import(string $title, string $body, string $assignee, array $labels)
+    {
+        $ch = curl_init($this->Url);
+        $payload = [
+            'title' => $title,
+            'body' => $body,
+            'assignees' => [$assignee],
+            'labels' => $labels
+        ];
+
+        $header = [
+            'Authorization: token ' . $this->Token,
+            'User-Agent: github-issues-importer-by-csv'
+        ];
+
+        $option = [
+            CURLOPT_HTTPHEADER => $header,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_TIMEOUT => 3
+        ];
+
+        curl_setopt_array($ch, $option);
+        curl_exec($ch);
+    }
+}
+
+$issues_importer = new IssuesImporter(
+    getenv('GITHUB_TOKEN'),
+    getenv('GITHUB_REPOSITORY')
+);
+
+$file_name = trim(fgets(STDIN));
+if ($file_name) {
+    $csv_file = new SplFileObject($file_name);
+    while (!$csv_file->eof()) {
+        $arr = $csv_file->fgetcsv();
+        if (!is_null($arr)) {
+            $labels = [];
+            for ($i = 3; $i <= count($arr) - 1; $i++) {
+                $labels[] = $arr[$i];
+            }
+            $issues_importer->import($arr[0], $arr[1], $arr[2], $labels);
+        }
+    }
+}
+
