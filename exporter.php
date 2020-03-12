e6b20de3949644d649c935498e14695b19a0cb59
iridon0920 <iridon0920@gmail.com>
2020-03-12 15:58:50 +0900

    追加:エクスポート実行時にissueの状態を選択する機能

--- exporter.php
+++ exporter.php
@@ -5,13 +5,22 @@ namespace App;
 require 'vendor/autoload.php';
 require 'common.php';
 
+echo "エクスポートするissueの状態を入力してください。 o(open) c(closed) other(all)\n";
+$input = trim(fgets(STDIN));
+if ($input === "o") {
+    $state = "open";
+} elseif ($input === "c") {
+    $state = "closed";
+} else {
+    $state = "all";
+}
 $result = [];
 $i = 1;
 while (1) {
     $github_connection = new Connection(
         getenv('GITHUB_TOKEN'),
         getenv('GITHUB_REPOSITORY'),
-        '/issues?page=' . $i
+        '/issues?page=' . $i . '&state=' . $state
     );
     $issues = $github_connection->export();
     $result = array_merge($result, $issues); 
@@ -24,9 +33,9 @@ while (1) {
 
 $csv = fopen("export.csv", "w");
 if ($csv) {
-    fputcsv($csv, ["id", "タイトル", "内容", "作成日", "クローズ日"]);
+    fputcsv($csv, ["id", "タイトル", "内容", "状態", "作成日", "クローズ日"]);
     foreach ($result as $val) {
-        fputcsv($csv, [$val->number, $val->title, $val->body, $val->created_at, $val->closed_at]);
+        fputcsv($csv, [$val->number, $val->title, $val->body, $val->state, $val->created_at, $val->closed_at]);
     }
     echo "CSVファイルの書き出しに成功しました。\n";
 } else {
